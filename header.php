b0570cd36b0cac06c90723c446af32e6fbe337a4
ronie0B03 <>
2021-02-28 14:15:48 +0800

    Update header.php

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 						<span class="user-name">Profile</span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-						<a class="dropdown-item" href="profile.html"><i class="dw dw-user1"></i> Profile</a>
+						<a class="dropdown-item" href="#"><i class="dw dw-user1"></i> Profile</a>
 						<a class="dropdown-item" href="login.php"><i class="dw dw-logout"></i> Log Out</a>
 
 					</div>
