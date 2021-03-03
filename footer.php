c2ed0f631ed81b37c4d9692cb96147f8c59b7b01
roniespeedster <ronieB03@gmail.com>
2021-03-03 21:07:10 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -59,8 +59,8 @@
   <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
   
   <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
+<!--  <script src="js/demo/chart-area-demo.js"></script>-->
+<!--  <script src="js/demo/chart-pie-demo.js"></script>-->
   <script src="js/demo/datatables-demo.js"></script>
 </body>
 
