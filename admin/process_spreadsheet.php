f287b5ddca80765681fe723c7ff8d35622dee9e7
perilloArt3159 <artperillo.ictdu@spcf.edu.ph>
2021-06-08 16:43:49 +0800

    :construction: Setup DOMPDF Php Package & Create Certificate Template (#13)

--- admin/process_spreadsheet.php
+++ admin/process_spreadsheet.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+require_once('vendor/autoload.php');
 require_once('dbh.php');
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
---
+++ pdf/test.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class='m-4'>
+        <header class='border'>
+            <div class='d-flex justify-content-between'>
+                <span>
+                    <img src="./acms.png" width='150px' height='150px'>
+                </span>
+                <article class='text-center border border-danger'>
+                    <h2>ANGELES CITY MEDICAL SOCIETY</h2>
+                    <p>Founded in 1972</p>
+                    <p>Component Society of the Philippine Medical Association</p>
+                </article>
+                <span>
+                    <img src="./pma.png" width='150px' height='150px' alt="pma logo">
+                </span>
+             </div>
+        </header>
+        <main class='mt-4'>
+            <h2 class='text-center font-weight-bold'>CERTIFICATE OF GOOD STANDING</h2>
+            <p class='text-justify text-height-2'>
+                This is to certify that <strong class="font-weight-bold">Name </strong> of the Angeles City Medical Society, a component of the <strong class="font-weight-bold">PHILIPPINE MEDICAL ASSOCIATION</strong>
+                , with PMA No. <strong class="font-weight-bold">PMA Number</strong> is a bonafide <strong class="font-weight-bold">MEMBER IN GOOD STANDING</strong> and is entitled to all the rights and privileges appertaining 
+                thereof. 
+            </p>
+            <p class='text-justify text-height-2'>
+                Membership dues for 2021-2022 have been settled and this certification is valid until May 31, 2021
+            </p>
+        </main>
+        <footer>
+            <div class='text-center'>
+                <strong>MICHAEL J. DIZON, MD.</strong>
+                <p>President</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
