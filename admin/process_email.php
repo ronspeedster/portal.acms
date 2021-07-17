65c664b06136300de42b27ccb9159039f352a172
roniespeedster <ronieB03@gmail.com>
2021-07-17 15:53:37 +0800

    :pencil: Remove dot after the middle name

--- admin/process_email.php
+++ admin/process_email.php
@@ -162,7 +162,7 @@ if(isset($_POST['verify_payment_certificate']))
     $signature  =   $cert['signature'];
 
     // $fullname   =  strtoupper($user['first_name'] . ' ' . substr($user['middle_name'], 0, 1) . '.' . ' ' . $user['last_name']);
-    $fullname   =  strtoupper($user['first_name'] . ' ' . $user['middle_name'] . '.' . ' ' . $user['last_name']);
+    $fullname   =  strtoupper($user['first_name'] . ' ' . $user['middle_name'] . ' ' . $user['last_name']);
     //* Add a recipient
 
     if(!empty($user['email']))
