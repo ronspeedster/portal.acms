b00a6cb87b0bbb2fd297ebb9818d32218f0d14c9
perilloArt3159 <artperillo.ictdu@spcf.edu.ph>
2021-07-31 16:20:45 +0800

    :passport_control: Reset Password Implementation (#26)

--- password_reset.php
+++ password_reset.php
@@ -26,7 +26,7 @@
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link rel="icon" href="img/logo.png" sizes="16x16"> 
+  <link rel="icon" href="img/logo/acms.png" sizes="16x16">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
 <style>
@@ -41,61 +41,62 @@
 
 <body class="bg-gradient-primary">
 
-  <div class="container">
+  <div class="container d-flex justify-content-center">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card o-hidden border-0 shadow-lg my-5 w-50">
+          
           <!-- Alert Here -->
-          <?php
-            if(isset($_SESSION['registerError'])){
-          ?>
-          <div class="alert alert-danger alert-dismissible">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <?php
-              echo $_SESSION['registerError'];
-              unset($_SESSION['registerError']);
-            ?>
-          </div>
-          <?php
-            }
-          ?>
-          <!-- End Alert Here -->
-      <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="row">
-          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-          <div class="col-lg-7">
-            <div class="p-5">
-              <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Password Reset</h1>
+          <?php if(isset($_SESSION['errors'])): ?>
+            <?php foreach($_SESSION['errors'] as $error): ?> 
+              <div class="alert alert-danger alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <?=$error?> 
               </div>
-              <form class="user" method="post" action="process_registration.php">
-                <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control" id="exampleInputPassword" placeholder="Password" name="password1" required>
+            <?php endforeach ?> 
+            <?php unset($_SESSION['errors'])?> 
+          <?php endif ?> 
+          
+          <?php if(isset($_SESSION['message'])): ?>
+              <div class="alert alert-success alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <?=$_SESSION['message']?> 
+              </div>
+          <?php unset($_SESSION['message'])?> 
+          <?php endif ?> 
+          <!-- End Alert Here -->          
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+<!--              <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>-->
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-2">Reset Password</h1>
+                    <p class="mb-4">Enter your email Address and confirm your new Password Below</p>
                   </div>
-                  <div class="col-sm-6">
-                    <input type="password" class="form-control form-control" id="exampleRepeatPassword" placeholder="Repeat Password" name="password2" required>
-                    <input type="hidden" name="token" value="<?php echo $token; ?>">
-                  </div>
+                  <form class="user" action="process_reset_password.php" method="POST">
+                    <input type="hidden" name="_token" value="<?=$token?>">
+                    <div class="form-group"> 
+                      <input type="email" class="form-control form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email" required>
+                    </div>
+                    <div class="form-group"> 
+                      <input type="password" class="form-control form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Password" name="password" required>
+                    </div>
+                    <div class="form-group"> 
+                      <input type="password" class="form-control form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Confirm Password" name="password_confirm" required>
+                    </div>
+
+                    <button class="btn btn-success btn-block" name="reset_password">
+                      Reset Password
+                    </button>
+                  </form>
                 </div>
-                <button type="submit" class="btn btn-success btn-block" name="new_password">
-                  Reset Password
-                </button>
-              </form>
-              <hr>
-              <div class="text-center">
-                <a class="small" href="forgot-password.php">Forgot Password?</a>
-              </div>
-              <div class="text-center">
-                <a class="small" href="login.php">Already have an account? Login!</a>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </div>
 
-  </div>
 
   <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
